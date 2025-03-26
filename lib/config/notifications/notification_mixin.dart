d569b0bfa8a53f3cf10cfb27dbfec5f323177d9b
Mahmoud Ashraf <mahmoud.ashraf.elkafrawy@gmail.com>
2025-03-26 13:34:00 +0200

    set firebase completed

--- lib/config/notifications/notification_mixin.dart
+++ lib/config/notifications/notification_mixin.dart
@@ -1,28 +1,28 @@
-// import 'dart:async';
-//
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter/material.dart';
-//
-// /// This mixin can listen to incoming notification
-// mixin FCMNotificationMixin<T extends StatefulWidget> on State<T> {
-//   late StreamSubscription<RemoteMessage> _subscription;
-//
-//   @override
-//   void initState() {
-//     _subscription = FirebaseMessaging.onMessage.listen(_onNewNotify);
-//     super.initState();
-//   }
-//
-//   @override
-//   void dispose() {
-//     _subscription.cancel();
-//     super.dispose();
-//   }
-//
-//   /// Will be called whenever a new notification come and app is in foreground
-//   void onNotify(RemoteMessage notification);
-//
-//   void _onNewNotify(RemoteMessage notification) {
-//     if (mounted) onNotify(notification);
-//   }
-// }
+import 'dart:async';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+/// This mixin can listen to incoming notification
+mixin FCMNotificationMixin<T extends StatefulWidget> on State<T> {
+  late StreamSubscription<RemoteMessage> _subscription;
+
+  @override
+  void initState() {
+    _subscription = FirebaseMessaging.onMessage.listen(_onNewNotify);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  /// Will be called whenever a new notification come and app is in foreground
+  void onNotify(RemoteMessage notification);
+
+  void _onNewNotify(RemoteMessage notification) {
+    if (mounted) onNotify(notification);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'config/clients/storage/storage_client.dart';
+import 'config/helpers/logging_helper.dart';
+import 'config/notifications/notifications_service.dart';
+import 'firebase_options.dart';
 import 'megaplug_app.dart';
 
 class MyHttpOverrides extends HttpOverrides {
@@ -33,5 +38,18 @@ void main() async {
 
   await StorageClient().init();
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await NotificationsService().init();
+  FirebaseMessaging.onBackgroundMessage(_firebaseBackgroundMessage);
+
   runApp(const MegaPlug());
+}
+
+Future _firebaseBackgroundMessage(RemoteMessage remoteMessage) async {
+  if (remoteMessage.notification != null) {
+    AppLogger.log('Background Notification Received.');
+  }
 }
